89aead4b9ed1bc8ad51c090992c135b34fe2c822
areco93 <pedroareco099@gmail.com>
2016-09-17 13:36:50 -0400

    formulario completo(corregir lo de admin y member)

--- services/resources/views/admin/users/create.blade.php
+++ services/resources/views/admin/users/create.blade.php
@@ -1,3 +1,4 @@
+<!-- REVISAR DOCUMENTACION DE LARAVEL COLLECTIVE -->
 @extends('admin.template.main')
 
 @section('title', 'Crear Usuario')
@@ -8,12 +9,40 @@
 
 
 
-	<div class="form-group">
-		{!! Form::label('name', 'Nombre') !!}
-		{!! Form::text('name',null, ['class' => 'form-control', 'placeholder' => 'Nombre completo', 'required']) !!}
+		<div class="form-group">
+			{!! Form::label('name', 'Nombre') !!}
+			{!! Form::text('name',null, ['class' => 'form-control', 'placeholder' => 'Nombre completo', 'required']) !!}
 
-	</div>
+		</div>
 
-    	
+		<div class="form-group">
+			{!! Form::label('email', 'Correo Electronico') !!}
+			{!! Form::text('email',null, ['class' => 'form-control', 'placeholder' => 'example@gmail.com', 'required']) !!}
+
+		</div>
+
+			<div class="form-group">
+			{!! Form::label('password', 'Contraseña') !!}
+			{!! Form::password('password', ['class' => 'form-control', 'placeholder' => '***********', 'required']) !!}
+
+		</div>
+
+		<div class="form-group">
+
+		{!! Form::label('type', 'Tipo') !!}
+		{!! Form::select('type', ['' => 'Seleccionar..', 'member' => 'Miembro', 'admin' => 'Administrador'], null, ['class' => 'form-control']) !!}
+
+		</div>
+
+		<div class="form-control">
+
+		{!! Form::submit('Registar', ['class' => 'btn btn-primary']) !!}
+
+		</div>
+
+
+
+
+
 	{!! Form::close() !!}
 @endsection
